f4b5e90bb6b1aa5b62dc2822c5439499e5e247b0
Евгений Ляшенко <eugene.coding@gmail.com>
2021-12-03 11:55:19 +0300

    Добавьте файлы проекта.

---
+++ VstaRYZVSfqO30OwTLGF3oS9TA/Macro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Corel.Interop.VGCore;
+
+namespace CBox
+{
+	public partial class Main
+	{
+		private void Startup()
+		{
+		}
+
+		[CgsAddInMacro]
+		public void Macro1()
+		{
+			MessageBox.Show("Macro1 - " + app.Name);
+		}
+	}
+}
